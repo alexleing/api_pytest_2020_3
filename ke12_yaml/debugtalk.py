6dd96dace850e1d03f7be496e7909019189a753f
alexleing <lechunxiaing@163.com>
2020-06-09 10:54:23 +0800

    更新requirements

--- ke12_yaml/debugtalk.py
+++ ke12_yaml/debugtalk.py
@@ -1,19 +1,17 @@
-import  requests
+import requests
 
 
 host = "http://49.235.92.12:6009"
 
+
 def get_token(username="test", psw="123456"):
-    body = {
-        "username": username,
-        "password": psw
-    }
+    body = {"username": username, "password": psw}
     s = requests.session()
-    r = s.post(host+"/api/vi/login", json=body)
+    r = s.post(host + "/api/vi/login", json=body)
     token = r.json()["token"]
     return token
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     t = get_token()
     print(t)
